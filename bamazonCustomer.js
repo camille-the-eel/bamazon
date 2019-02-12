cc4a68854281252e222b4c105924a9b61f7636b8
Camille <c.hughes64@gmail.com>
2019-02-12 14:10:23 -0500

    response to userPrompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,30 +46,43 @@ function userPrompt() {
             message: "What is the Item ID of the product you would like to purchase?"
             },
             {
-            name: "purchase_id",
+            name: "purchase_amount",
             type: "input",
             message: "How many units would you like to purchase?"
             }
         ])
-        .then(function(answer){
-            var item = connection.query("SELECT item_id FROM products");
-            
-            if (answer.purchase_id === item) {
-                var purchasing = item;
-                console.log(purchasing);
-            } else {
-                console.log("This item does not exist.");
-            }
-            
-            connection.query()
-        });
+        .then(function(answer) {
 
-    //inquirer 
-    //id of product to buy & # of units
-    storeCheck();
+            var item = answer.purchase_id;
+            var units = answer.purchase_amount;
+
+            connection.query(
+                "SELECT product_name, price, stock_quantity FROM products WHERE item_id=?", [item], function (err,res) {
+
+                if (err) {
+                    console.log("Item ID does not exist or invalid input submitted.");
+                    throw err;
+                }
+
+                var purchasing = res[0].product_name;
+                var cost = res[0].price;
+                var stock = res[0].stock_quantity;
+        
+                if (stock >= units) {
+                    var purchaseQty = units;
+                    var total = purchaseQty * cost;
+                    console.log("Item #" + item + ": " + purchasing + ", has been added to your cart. Quantity: " + purchaseQty + ". Your total is $" + total + ".");
+                    storeUpdate();
+                } else {
+                    console.log("We're sorry, we could not complete your order. There are only " + stock + " units available of Item #" + item + ": " + purchasing + ".");
+                    connection.end();
+                }
+
+            });
+        });
 }
 
-function storeCheck() {
+function storeUpdate() {
     //enough in stock
         //update mysql quantity
     //show customer total cost of purchase
